de3d690d0ad0103ddfa40481396baeb6b7182cd2
rabia çakıcı <rabia.cakiciiii@gmail.com>
2025-07-08 13:02:54 +0300

    Initial Commit

---
+++ AnimalMatchGame/AnimalMatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalMatchGameApp.swift
+//  AnimalMatchGame
+//
+//  Created by Rabia Çakıcı on 8.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimalMatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalMatchGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimalMatchGame
+//
+//  Created by Rabia Çakıcı on 8.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
